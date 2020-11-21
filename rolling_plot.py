22be647306a398eeadf6f71d69d66206d240498e
unknown <ocanath@gmail.com>
2020-11-20 19:09:34 -0600

    can get data inside rolling plot, but can't plot it

--- rolling_plot.py
+++ rolling_plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 import serial
+import getfloat
 
 start_time = time.time()
 
@@ -13,19 +14,28 @@ line, = ax.plot(x, np.sin(x))
 ax.set_ylim(-2,2)
 xdata, ydata = [0]*623, [0]*623
 
+#TODO: put initialization (and other parameters) in an init function and call rolling_plot outside of this file
+ser = serial.Serial('COM4', 921600, timeout = 1)
+
 
 def init():  # only required for blitting to give a clean slate.
     line.set_ydata([np.nan] * len(x))
     return line,
 
-
 def animate(data):
     #line.set_ydata(np.sin(i/50) )  # update the data.
     t = time.time()-start_time
+
+
     del xdata[0]
     del ydata[0]
     xdata.append(t)
+    
+    f_arr = getfloat.get_floats(ser,2)
+    print (f_arr)
+    #ydata.append(f_arr[0])   #for now, only first element is plotted. add more later
     ydata.append(np.sin(t*2*np.pi))
+    
     ax.relim()
     ax.autoscale_view()
     line.set_data(xdata,ydata)
@@ -46,3 +56,5 @@ ani = animation.FuncAnimation(
 # ani.save("movie.mp4", writer=writer)
 
 plt.show()
+    
+ser.close()
--- sertest.py
+++ sertest.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 import serial
-import struct
 import getfloat
 
 ser = serial.Serial('COM4', 921600, timeout = 1)
 print(ser.name)
 
+xdat = [0]*500
+
 count = 0
 while (count < 500):
-    f_arr = getfloat.get_floats(ser,2);
+    f_arr = getfloat.get_floats(ser,2)
     print(f_arr)
+    del xdat[0]
+    xdat.append(f_arr[0])
     count = count + 1
     
 ser.close()
