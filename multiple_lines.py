ae19580a9aab9460415cb75a7a9e3e9580cb66d3
unknown <ocanath@gmail.com>
2020-12-02 23:12:29 -0600

    almost ready for merge multiple_lins and the com orllign lines python scrip

--- multiple_lines.py
+++ multiple_lines.py
@@ -12,12 +12,12 @@ baud = 921600
 timeout = 1
 ylower = -2
 yupper = 2
-bufwidth = 500
-num_lines = 2
+bufwidth = 100
+num_lines = 10
 
 fig, ax = plt.subplots()
 
-x = np.arange(0, bufwidth, 0.01)
+x = np.arange(0, bufwidth, 1)
 #line, = ax.plot([], [])
 
 #line, = ax.plot(x, np.sin(x))
@@ -27,57 +27,48 @@ x = np.arange(0, bufwidth, 0.01)
 #line2, = ax.plot(x,np.sin(x))
 lines = []
 for i in range(0,num_lines):
-	lines.append(ax.plot(x,np.sin(x)))
+    lines.append(ax.plot(x,np.sin(x)))
 
 ax.set_ylim(ylower,yupper)
 
 xbuf = []
 ybuf = []
 for i in range(0,num_lines):
-	xbuf.append([])
-	ybuf.append([])
+    xbuf.append([])
+    ybuf.append([])
 for i in range(0, num_lines):
-	for j in range(0,bufwidth):
-		xbuf[i].append(0)
-		ybuf[i].append(0)
+    for j in range(0,bufwidth):
+        xbuf[i].append(0)
+        ybuf[i].append(0)
 
 #def init():  # only required for blitting to give a clean slate.
-	#for i in lines:
-	#	lines[i].set_data(x,np.sin(x))
-	#return line,
-	
+    #for i in lines:
+    #   lines[i].set_data(x,np.sin(x))
+    #return line,
+    
 
 def animate(data):
 
-	t = time.time()-start_time
+    t = time.time()-start_time
 
-	del xbuf[0][0]
-	del ybuf[0][0]
-	xbuf[0].append(t)
-	ybuf[0].append(np.sin(t*2*np.pi))
+    for i in range(0,num_lines):
+        del xbuf[i][0]
+        del ybuf[i][0]
+        xbuf[i].append(t)
+        ybuf[i].append(np.sin(t*2*np.pi - i/2))
 
-	del xbuf[1][0]
-	del ybuf[1][0]
-	xbuf[1].append(t)
-	ybuf[1].append(np.sin(t*2*np.pi - 1.2))
+    ax.relim()
+    ax.autoscale_view()
 
-	ax.relim()
-	ax.autoscale_view()
-
-	#line.set_data(xbuf[0],ybuf[0])
-	for i in range(0,num_lines):
-		line, = lines[i]
-		line.set_data(xbuf[i],ybuf[i])
-		
-	#for i in range(0,num_lines):
-	#	lines[i].set_data(xbuf[lnum],ybuf[lnum])
-	#lines[0].set_data(xbuf[0],ybuf[0])
-
-	#return line,
+    #line.set_data(xbuf[0],ybuf[0])
+    for i in range(0,num_lines):
+        line, = lines[i]
+        line.set_data(xbuf[i],ybuf[i])
+        
 
 
 ani = animation.FuncAnimation(
-    fig, animate, interval=2, blit=False, save_count=50)
+    fig, animate, interval=2, blit=False, save_count=10)
 
 # To save the animation, use e.g.
 #
