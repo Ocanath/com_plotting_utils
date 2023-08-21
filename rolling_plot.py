7277b91c0f6357a501f10d7b745b7d0f044611c5
ocanath <ocanath@gmail.com>
2023-08-21 14:02:56 -0700

    updates to plot

--- rolling_plot.py
+++ rolling_plot.py
@@ -7,63 +7,45 @@ import getfloat
 
 start_time = time.time()
 
-port = 'COM4'
-baud = 921600
-timeout = 1
 ylower = -2
 yupper = 2
-bufwidth = 500
-num_lines = 10
-
-ser = serial.Serial('COM4', 921600, timeout = 1)    #set up serial port
+bufwidth = 200
+num_lines = 3
 
 fig, ax = plt.subplots()
 ax.set_ylim(ylower,yupper)
 
 lines = []
 for i in range(num_lines):
-    lines.append(ax.plot([],[])[0])
+	lines.append(ax.plot([],[])[0])
 
-xbuf = []
-ybuf = []
-for i in range(0,num_lines):
-    xbuf.append([])
-    ybuf.append([])
-for i in range(0, num_lines):
-    for j in range(0,bufwidth):
-        xbuf[i].append(0)
-        ybuf[i].append(0)
-
-def init():  # only required for blitting to give a clean slate.
-    for line in lines:
-       line.set_data([],[])
-    return lines
-
+xbuf = np.zeros(bufwidth)
+ybuf = np.zeros((num_lines, bufwidth))
 
 def animate(data):
+	global xbuf
+	global ybuf
 
-    t = time.time()-start_time
-    
-    f_arr = getfloat.get_floats(ser,num_lines)
-    #print(f_arr) #optional print to console of the floating data we got
-    
-    for i in range(0,num_lines):
-        del xbuf[i][0]
-        del ybuf[i][0]
-        xbuf[i].append(t)
-        ybuf[i].append(float(*f_arr[0]))
+	t = time.time()-start_time
+	xbuf = np.roll(xbuf,1)	#roll xbuf by 1
+	xbuf[0] = t	#load in new value
+			
+	for i in range(0,num_lines):
+		ybuf[i] = np.roll(ybuf[i],1) #roll 1
+		ybuf[i][0] = np.sin(t*2*np.pi + i)	#load new value
 
-    ax.relim()
-    ax.autoscale_view()
+		
+	ax.relim()
+	ax.autoscale_view()
 
-    for i, line in enumerate(lines):
-        line.set_data(xbuf[i],ybuf[i])
-    
-    return lines
+	for i, line in enumerate(lines):
+		line.set_data(xbuf,ybuf[i])
+   
+	return lines
 
 
 ani = animation.FuncAnimation(
-    fig, animate, init_func=init, interval=2, blit=True, save_count=50)
+	fig, animate, init_func=None, interval=1, blit=False, save_count=None)
 
 # To save the animation, use e.g.
 #
@@ -76,5 +58,4 @@ ani = animation.FuncAnimation(
 # ani.save("movie.mp4", writer=writer)
 
 plt.show()
-    
-ser.close()
+
